72501b015e9bf07ff36a936da193503ce1c0da5d
hookex <cookmycode@gmail.com>
2020-03-22 20:14:53 +0800

    AnimatedValue setValue view update no trigger

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
         <div className="App">
             <header className="App-header">
                 {/*<MapViewer/>*/}
-                <WithSpring/>
-                {/*<SpringWithDom/>*/}
+                {/*<WithSpring/>*/}
+                <SpringWithDom/>
             </header>
         </div>
     );
@@ -40,4 +40,14 @@ AnimatedObject.ts:12 payload Set(1) {AnimatedValue}
 
 animated {name: "sphere1", diameter: 2, segments: 16, position: Vector3}name: "sphere1"diameter: 2segments: 16position: Vector3 {x: AnimatedValue, y: 1, z: 0}__proto__: Object
 AnimatedObject.ts:12 payload Set(0) {}
+
+
+collectViews up AnimatedValue {children: Set(1), views: Set(0), done: false, value: 0, payload: Set(1), …} Set(0) {}
+AnimatedArray.ts:8 collectViews inner AnimatedValue {children: Set(1), views: Set(0), done: false, value: 0, payload: Set(1), …} Set(0) {} undefined
+AnimatedArray.ts:8 collectViews inner AnimatedStyle {children: Set(1), source: {…}, payload: Set(1), Symbol(isAnimated): true} Set(0) {} undefined
+AnimatedArray.ts:8 collectViews inner AnimatedProps {children: Set(0), source: {…}, payload: Set(1), Symbol(isAnimated): true, update: ƒ} Set(0) {} () => {
+    console.log('node', node);
+    if (!node.current) return;
+    const didUpdate = Object(_react_spring_shared_globals__WEBPACK_IMPORTED_MODULE_2__["applyAnimatedValues"])(node.current, next…
+AnimatedValue.ts:46 collectViews down AnimatedValue {children: Set(1), views: Set(1), done: false, value: 0, payload: Set(1), …} Set(1) {AnimatedProps}
  */
